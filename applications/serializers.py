7b4cf31aa1eaf8fb109634c7617e2a352218022c
HusenKomilov <komilovhusen73@gmail.com>
2025-08-21 18:57:20 +0500

    fix: connection create  application hash

--- applications/serializers.py
+++ applications/serializers.py
@@ -9,6 +9,17 @@ class ApplicationSerializer(serializers.ModelSerializer):
 
 
 class ConnectionSerializer(serializers.ModelSerializer):
+    application_hash = serializers.CharField(write_only=True)
+
     class Meta:
         model = Connection
-        fields = '__all__'
+        fields = ['id', 'application_hash', 'protocol', 'direction', 'local_address', 'remote_address', 'status', 'more_info']
+
+    def create(self, validated_data):
+        app_hash = validated_data.pop('application_hash')
+        try:
+            application = Application.objects.get(hash=app_hash)
+        except Application.DoesNotExist:
+            raise serializers.ValidationError({'application_hash': 'Invalid application hash'})
+        connection = Connection.objects.create(application=application, **validated_data)
+        return connection
