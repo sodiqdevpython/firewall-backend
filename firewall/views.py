5804afe15faf4fbd376e7881c55b0cd8f04b65d2
thequlmatov <zuxroka0104@gmail.com>
2025-08-23 14:46:20 +0500

    fix: db

--- firewall/views.py
+++ firewall/views.py
@@ -1,7 +1,14 @@
-from rest_framework import viewsets
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from .models import FirewallRule, RuleAssignment
 from .serializers import FirewallRuleSerializer, RuleAssignmentSerializer
-from firewall.services.firewall_rule_service import FirewallRuleService
+from hosts.models import Device
+from applications.models import Application
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 
 class FirewallRuleViewSet(viewsets.ModelViewSet):
@@ -12,8 +19,48 @@ class FirewallRuleViewSet(viewsets.ModelViewSet):
     ordering_fields = ['port', 'protocol', 'direction']
 
     def create(self, request, *args, **kwargs):
-        firewall_rule = FirewallRuleService.create_firewall_rule(request.data.copy())
+        data = request.data.copy()
 
+        host_data = data.get("host")
+        application_hash = data.get("application_hash")
+
+        if isinstance(host_data, dict):
+            host_name = host_data.get("host_name")
+            try:
+                device = Device.objects.get(host_name=host_name)
+            except Device.DoesNotExist:
+                raise ValidationError("Device with the given host_name does not exist.")
+        else:
+            try:
+                device = Device.objects.get(bios_uuid=host_data)
+            except Device.DoesNotExist:
+                raise ValidationError("Device with the given BIOS UUID does not exist.")
+
+        if not application_hash:
+            raise ValidationError("Application hash is required.")
+
+        try:
+            application = Application.objects.get(hash=application_hash)
+        except Application.DoesNotExist:
+            raise ValidationError("Application with the given hash does not exist.")
+
+        firewall_rule = FirewallRule.objects.create(
+            host=device,
+            application=application,
+            **{k: v for k, v in data.items() if k not in ["host", "application_hash"]}
+        )
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            f"device_{firewall_rule.host.bios_uuid}",
+            {
+                "type": "firewall.rule",
+                "rule": json.loads(json.dumps(FirewallRuleSerializer(firewall_rule).data, cls=DjangoJSONEncoder)),
+            }
+        )
+
+        serializer = self.get_serializer(firewall_rule)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class RuleAssignmentViewSet(viewsets.ModelViewSet):
