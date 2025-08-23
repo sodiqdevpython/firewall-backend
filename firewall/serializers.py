c088660ad6d48c95936f26c83c6c5fbe25a9f06e
HusenKomilov <komilovhusen73@gmail.com>
2025-08-23 16:30:45 +0500

    Revert "fix: application hash"
    This reverts commit 332f836cdb2a07925088e447a03cb8d823659e78.

--- firewall/serializers.py
+++ firewall/serializers.py
@@ -5,22 +5,22 @@ from .models import FirewallRule, RuleAssignment
 
 
 class FirewallRuleSerializer(serializers.ModelSerializer):
-    hash = serializers.CharField(write_only=True)
+    application_hash = serializers.CharField(write_only=True)
     host = DeviceNestedSerializer(read_only=True)
 
     class Meta:
         model = FirewallRule
-        fields = ("id", "host", "hash", "application",
+        fields = ("id", "host", "application_hash", "application",
                   "port", "protocol", "direction", "action")
 
     def create(self, validated_data):
-        app_hash = validated_data.pop("hash", None)
+        app_hash = validated_data.pop("application_hash", None)
         try:
             application = Application.objects.filter(hash=app_hash).first()
             # device = device  # Just to avoid unused variable warning
         except Application.DoesNotExist:
             raise serializers.ValidationError(
-                {'hash': 'Invalid application hash'})
+                {'application_hash': 'Invalid application hash'})
         validated_data['application'] = application
         return super().create(validated_data)
 
