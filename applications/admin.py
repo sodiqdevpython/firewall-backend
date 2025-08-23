76c4bbf345a9bac7add5a41662aff48b58db23b8
unknown <komilovhusen73@gmail.com>
2025-08-23 17:08:35 +0500

    fix: pass

--- applications/admin.py
+++ applications/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Application, Connection
+from hosts.models import Device
+
 
 @admin.register(Application)
 class ApplicationAdmin(admin.ModelAdmin):
@@ -7,7 +9,23 @@ class ApplicationAdmin(admin.ModelAdmin):
     list_filter = ("status", "host")
     search_fields = ("name", "pid", "hash")
 
+
+class HostNameFilter(admin.SimpleListFilter):
+    title = "Host Name"
+    parameter_name = "host_name"
+
+    def lookups(self, request, model_admin):
+        hosts = set(Device.objects.values_list("id", "host_name"))
+        return [(id, name) for id, name in hosts]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(application__host__id=self.value())
+        return queryset
+
+
 @admin.register(Connection)
 class ConnectionAdmin(admin.ModelAdmin):
     list_display = ("application", "local_address", "remote_address",)
     search_fields = ("local_address", "remote_address")
+    list_filter = ("application__host",)
