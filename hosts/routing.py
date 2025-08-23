6198bfd5d30888d3078920beca364d03b8c70f8a
HusenKomilov <komilovhusen73@gmail.com>
2025-08-23 14:42:08 +0500

    feat: connection for firewall

--- hosts/routing.py
+++ hosts/routing.py
@@ -3,4 +3,6 @@ from hosts import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/device/(?P<bios_uuid>[0-9a-f-]+)/$', consumers.AgentConsumer.as_asgi()),
+    re_path(r"ws/device/fire_wall/(?P<bios_uuid>[^/]+)/$", consumers.DeviceConsumer.as_asgi()),
+
 ]
