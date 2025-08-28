095c37f49593aafeb904b9c902ae0b0eadeaf3db
HusenKomilov <komilovhusen73@gmail.com>
2025-08-28 12:36:37 +0500

    fix: updated_id in patch

--- patch/serializers.py
+++ patch/serializers.py
@@ -11,7 +11,7 @@ class PatchManagementSerializer(serializers.ModelSerializer):
         fields = [
             "id", "title", "support", "kb",
             "mandatory", "reboot_required", "downloaded",
-            "device", "device_bios_uuid", "download_patch", "install_patch"
+            "device", "device_bios_uuid", "download_patch", "install_patch", "update_id"
         ]
         read_only_fields = ["device"]
 
